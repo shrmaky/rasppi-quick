96df3e6b68ff2519474fe2e1a5c4c093dfcd82ad
Akshat Sharma <akshatsharma@Akshats-MBP.TAJAWAL.LOCAL>
2018-07-29 11:57:20 +0400

    Barcode Generator

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CameraApp from './Camera';
+import WebcamCapture from "./Webcam";
 
 export default function Main () {
-  return <CameraApp/>
+  return <WebcamCapture/>
 }
